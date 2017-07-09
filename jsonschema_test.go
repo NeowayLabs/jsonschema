a4aae5404110891914087121dd05ba3131dbbe94
Matheus Vill <matheusvill@gmail.com>
2017-07-09 19:04:51 -0300

    Add support to int field

--- jsonschema_test.go
+++ jsonschema_test.go
@@ -4,16 +4,16 @@ import (
 	"testing"
 )
 
-func TestCheckUsingFieldStringShouldReturnTrueWhenTypeIsString(t *testing.T) {
+func TestCheckUsingFieldStringShouldReturnTrue(t *testing.T) {
 
 	schema := map[string]interface{}{
-		"name": map[string]interface{}{
+		"stringField": map[string]interface{}{
 			"type": "string",
 		},
 	}
 
 	data := map[string]interface{}{
-		"name": "name",
+		"stringField": "name",
 	}
 
 	expected := true
@@ -27,13 +27,53 @@ func TestCheckUsingFieldStringShouldReturnTrueWhenTypeIsString(t *testing.T) {
 func TestCheckUsingFieldStringShouldReturnFalseWhenTypeIsntString(t *testing.T) {
 
 	schema := map[string]interface{}{
-		"name": map[string]interface{}{
+		"stringField": map[string]interface{}{
 			"type": "string",
 		},
 	}
 
 	data := map[string]interface{}{
-		"name": 1,
+		"stringField": 1,
+	}
+
+	expected := false
+	actual := Check(data, schema)
+
+	if actual != expected {
+		t.Error("Test failed. Expected", expected, "but returned", actual)
+	}
+}
+
+func TestCheckUsingFieldIntShouldReturnTrue(t *testing.T) {
+
+	schema := map[string]interface{}{
+		"intField": map[string]interface{}{
+			"type": "int",
+		},
+	}
+
+	data := map[string]interface{}{
+		"intField": 1,
+	}
+
+	expected := true
+	actual := Check(data, schema)
+
+	if actual != expected {
+		t.Error("Test failed. Expected", expected, "but returned", actual)
+	}
+}
+
+func TestCheckUsingFieldIntShouldReturnFalseWhenTypeIsntInt(t *testing.T) {
+
+	schema := map[string]interface{}{
+		"intField": map[string]interface{}{
+			"type": "int",
+		},
+	}
+
+	data := map[string]interface{}{
+		"intField": "1",
 	}
 
 	expected := false
