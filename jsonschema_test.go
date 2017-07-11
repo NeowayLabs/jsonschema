27d4974eaa3efb7db32a29a54085cbca6881e199
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-07-11 08:13:01 -0300

    Add nested object success

--- jsonschema_test.go
+++ jsonschema_test.go
@@ -160,6 +160,32 @@ func TestSuccessOn(t *testing.T) {
 			success: true,
 		},
 		Scenario{
+			name: "NestedObject",
+			data: `{
+				"objectField": {
+					"nestedObject" : {
+						"stringField" : "name"
+					}
+				}
+			}`,
+			schema: `{
+				"objectField": {
+					"type" : "object",
+					"format" : {
+						"nestedObject" : {
+							"type" : "object",
+							"format" : {
+								"stringField" : {
+									"type" : "string"
+								}
+							}
+						}
+					}
+				}
+			}`,
+			success: true,
+		},
+		Scenario{
 			name: "FloatField",
 			data: `{
 				"floatField": 1.3
@@ -177,40 +203,6 @@ func TestSuccessOn(t *testing.T) {
 		testScenario(t, scenario)
 	}
 }
-
-//func TestCheckUsingMultipleFieldObjectShouldReturnTrue(t *testing.T) {
-
-//schema := map[string]interface{}{
-//"objectField": map[string]interface{}{
-//"type": "object",
-//"format": map[string]interface{}{
-//"secondObjectField": map[string]interface{}{
-//"type": "object",
-//"format": map[string]interface{}{
-//"stringField": map[string]interface{}{
-//"type": "string",
-//},
-//},
-//},
-//},
-//},
-//}
-
-//data := map[string]interface{}{
-//"objectField": map[string]interface{}{
-//"secondObjectField": map[string]interface{}{
-//"stringField": "field",
-//},
-//},
-//}
-
-//expected := true
-//actual := jsonschema.Check(data, schema)
-
-//if actual != expected {
-//t.Error("Test failed. Expected", expected, "but returned", actual)
-//}
-//}
 
 //func TestCheckUsingFieldArrayShouldReturnTrue(t *testing.T) {
 
